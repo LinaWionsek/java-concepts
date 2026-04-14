128e1087d43e6bbdd5306f836591f218f890bb50
Lina Wionsek <lwionsek@wbsedu.de>
2026-04-14 11:14:47 +0200

    added module structured programming

---
+++ 01-structured-programming/day-02/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var höhe = 3;
+        var breite = -1;
+        if (höhe > 0 && breite < 0) {
+            System.out.println("Alles gut!");
+        } else {
+            System.out.println("ERROR");
+        }
+
+    }
+}
