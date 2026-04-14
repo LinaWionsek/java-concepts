8f27f4604a8e9f0755be7d6594c10e29aabfd8ca
Lina Wionsek <lwionsek@wbsedu.de>
2026-04-14 12:54:16 +0200

    added content of day 2

--- 01-structured-programming/day-02/src/Schleife.java
+++ 01-structured-programming/day-02/src/Schleife.java
@@ -1,18 +1,21 @@
 public class Schleife {
     public static void main(String[] args) {
         var startwert = 1;
-        var endwert = 1000;
-        var zähler = startwert;
+        var endwert = 10;
 
-        while(zähler <= endwert) { //solange die Bedingung true ist
+        // Kopfgesteuerte Schleife
+        var zähler = startwert;
+        while (zähler <= endwert) {
             System.out.println(zähler);
-            zähler++; // zähler = zähler +1
+            zähler++; //zähler = zähler + 1;
         }
+
         System.out.println("Alles fertig!");
 
-        // Alternative Zählschleife
-        for (var i = startwert; i <= endwert ; i++) {
-
+        // Alternative
+        // Zählschleife
+        for (var i = startwert; i <= endwert; i++) {
+            System.out.println(i);
         }
     }
 }
--- 01-structured-programming/day-02/src/Verzweigung.java
+++ 01-structured-programming/day-02/src/Verzweigung.java
@@ -1,14 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Verzweigung {
     public static void main(String[] args) {
         var höhe = 3;
         var breite = -1;
-        if (höhe > 0 && breite < 0) {
+
+        // verzweigung
+        if (höhe > 0 && breite > 0) {
             System.out.println("Alles gut!");
-        } else {
-            System.out.println("ERROR");
         }
+        else {
+            System.out.println("ERROR!");
+        }
+
         System.out.println("Tschüssi!");
     }
 }
